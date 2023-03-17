1953d870f882edb451a64c9fdd86b44307354a56
Shiva Ghanate <Shiva.Ghanate@lionbridge.com>
2023-03-16 20:03:18 -0400

    Add project files.

---
+++ Triangle/Diamond.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Diamond
+    {
+        public void DrawEquilateralDiamond(int height)
+        {
+            Console.WriteLine("Diamond drawing in progress");
+            int topHeight = (height / 2) + 1;
+            int bottomHeight = height / 2;
+            int spacesBeforeAsterisk = 0;
+            for (int i = 0; i <= topHeight; i++)
+            {
+                spacesBeforeAsterisk = (topHeight - i);
+                for (int j = 0; j < spacesBeforeAsterisk; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 0; k < i; k++)
+                {
+                    Console.Write(" *");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = bottomHeight; i > 0; i--)
+            {
+
+                spacesBeforeAsterisk = (bottomHeight - i);
+                for (int j = 0; j <= spacesBeforeAsterisk; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 0; k < i; k++)
+                {
+                    Console.Write(" *");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+    }
+}
---
+++ Triangle/Triangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Triangle
+    {
+        public void DrawEquilateralTriangle(int height)
+        {
+            Console.WriteLine("Triangle drawing in progress");
+            int spacesBeforeAsterisk;
+            for (int i = 1; i <= height; i++)
+            {
+                spacesBeforeAsterisk = (height - i);
+                for (int j = 0; j < spacesBeforeAsterisk; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 0; k < i; k++)
+                {
+                    Console.Write(" *");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
