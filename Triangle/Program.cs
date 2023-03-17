530e2e3d4306f2118b35d950a085917930b3f99f
Shiva Ghanate <Shiva.Ghanate@lionbridge.com>
2023-03-16 20:11:38 -0400

    Add a try catch

--- Triangle/Program.cs
+++ Triangle/Program.cs
@@ -7,20 +7,29 @@ namespace Shapes
     {
         static void Main(string[] args)
         {
-            Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
-            Console.WriteLine("How big of an equilateral Triangle?");
-            int triangleRows = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Number entered by user is {triangleRows}");
+            try
+            {
+                Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+                Console.WriteLine("How big of an equilateral Triangle?");
+                int triangleRows = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Number entered by user is {triangleRows}");
 
-            Triangle triangle = new Triangle();
-            triangle.DrawEquilateralTriangle(triangleRows);
+                Triangle triangle = new Triangle();
+                triangle.DrawEquilateralTriangle(triangleRows);
 
-            Console.WriteLine("How big of a Diamond, an odd number would be good?");
-            int diamondRows = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Number entered by user is {diamondRows}");
+                Console.WriteLine("How big of a Diamond, an odd number would be good?");
+                int diamondRows = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Number entered by user is {diamondRows}");
 
-            Diamond diamond = new Diamond();
-            diamond.DrawEquilateralDiamond(diamondRows);
+                Diamond diamond = new Diamond();
+                diamond.DrawEquilateralDiamond(diamondRows);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Advice add an integer number..!!");
+                throw ex;
+            }
+            
         }
     }
 }
